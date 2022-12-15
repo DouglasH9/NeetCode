9851833dc7904bf9a46a666683ae165d87fe302c
DouglasH9 <douglash9882@gmail.com>
2022-12-15 17:23:08 -0600

    product except self algo added

---
+++ ArraysAndHashing/ProductExceptSelf.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace ArraysAndHashing
+{
+    public static class ProductExceptSelf
+    {
+        public static int[] Calc(int[] nums)
+        {
+            int length = nums.Length;
+            int[] answerArr = new int[length];
+
+            
+
+            answerArr[0] = 1;
+            for (int i = 1; i < length; i++)
+            {
+                answerArr[i] = answerArr[i - 1] * nums[i - 1];
+            }
+
+            int right = 1;
+            for (int i = length - 1; i > -1; i--)
+            {
+                answerArr[i] = answerArr[i] * right;
+                right *= nums[i];
+            }
+
+            return answerArr;
+        }
+    }
+}
+
--- ArraysAndHashing/Program.cs
+++ ArraysAndHashing/Program.cs
@@ -1,5 +1,11 @@
 ﻿using ArraysAndHashing;
 
-int[] answer1 = TopKFreqElements.Calc(new int[] { 1, 1, 1, 2, 2, 3, }, 2);
-foreach (int num in answer1)
+//int[] answer1 = TopKFreqElements.Calc(new int[] { 1, 1, 1, 2, 2, 3, }, 2);
+//foreach (int num in answer1)
+//    Console.WriteLine(num);
+
+int[] answer = ProductExceptSelf.Calc(new int[] { 1, 2, 3, 4 });
+foreach (int num in answer)
+{
     Console.WriteLine(num);
+}
